4c6b50581baf7b81057823109028e017f3ba5cc0
dongzhijiaren <gogoxiaodong@126.com>
2017-10-30 16:59:57 +0800

    创建RNListView项目

---
+++ ListViewTest/index.ios.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import LView from './LView'
+import LView2 from './LView2'
+
+export default class ListViewTest extends Component {
+
+
+  render() {
+    return (
+     <LView2 />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ListViewTest', () => ListViewTest);
