9807e730fc48b2fa36a5fb6c3f25b6e870f7c20b
dongzhijiaren <gogoxiaodong@126.com>
2018-08-14 17:51:30 +0800

    添加斯斯小宝宝

--- ListViewTest/index.ios.js
+++ ListViewTest/index.ios.js
@@ -19,6 +19,7 @@ export default class ListViewTest extends Component {
 console.log('hello sir');
 console.log('上传时间2018-8-14 下午5点44份11秒');
 console.log('修改日期：2018-8-14 17:46:58');
+console.log('4,斯斯小宝宝');
   render() {
     return (
      <LView2 />
